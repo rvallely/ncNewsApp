d8c69e592b850ca4c4e2580185b97801bfb0621d
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-15 16:21:35 +0000

    attempt fix cors error

--- app.js
+++ app.js
@@ -8,13 +8,9 @@ const { handle404s, handleCustomErrors, handleServerErrors, handlePsqlErrors } =
 const endpoints = require('./endpoints');
 const cors = require('cors');
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
+app.use(cors(
+    { origin: 'http://localhost:3000' }
+));
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
