35f1128c42df7618627097ea11fb30cff9b21b84
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-15 16:00:22 +0000

    edit cors options to handle no header present error

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const { handle404s, handleCustomErrors, handleServerErrors, handlePsqlErrors } =
 const endpoints = require('./endpoints');
 const cors = require('cors');
 
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
